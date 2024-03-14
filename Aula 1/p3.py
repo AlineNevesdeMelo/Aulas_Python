bd87e12092afa142f96df85053e32e97511ada5f
AlineNevesdeMelo <160277352+AlineNevesdeMelo@users.noreply.github.com>
2024-03-14 14:35:12 -0300

    Update and rename p3.py to Aula 1/p3.py

---
+++ Aula 1/p3.py
@@ -0,0 +1,3 @@
+I=int(input('Qual sua idade: '))
+Ano=2024-I
+print('Ano aproximado de nascimento: '+str(Ano))
--- p3.py
+++
@@ -1,3 +0,0 @@
-I=int(input('Qual sua idade: '))
-Ano=2024-I
-print('Ano aproximado de nascimento: '+str(Ano))
