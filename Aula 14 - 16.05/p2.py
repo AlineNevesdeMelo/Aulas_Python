e7044196984cde37bc542011f3ee18aa5b0e0757
AlineNevesdeMelo <160277352+AlineNevesdeMelo@users.noreply.github.com>
2024-05-16 14:25:47 -0300

    Add files via upload

---
+++ Aula 14 - 16.05/p2.py
@@ -0,0 +1,7 @@
+#Teste, explique e tire print de toda a tela
+
+vet_misto = [10, 3.14, "Python", 25, 2.71, "Programação"]
+for elemento in vet_misto:
+    print(elemento)
+
+# O programa lê e mostra cada variável, independente do tipo, dentro do vetor.
---
+++ Aula 14 - 16.05/p3.py
@@ -0,0 +1,18 @@
+#Rode, teste, explique e tire print panorâmico
+
+meu_vetor = [10, 'Python', -10.55, 0.0001, 50]
+
+elemento_posicao_2 = meu_vetor[2]
+print(elemento_posicao_2)
+
+primeiro_elemento = meu_vetor[0]
+print(primeiro_elemento)
+
+ultimo_elemento = meu_vetor[-1]
+print(ultimo_elemento)  
+
+#FAÇA UM FOR PARA MOSTRAR TODOS OS ELEMENTOS
+for i in meu_vetor:
+    print(i)
+#Cada variável corresponde a um elemento dentro do vetor (array)
+#O laço "for" vai mostrar todos os elementos dentro do vetor.
